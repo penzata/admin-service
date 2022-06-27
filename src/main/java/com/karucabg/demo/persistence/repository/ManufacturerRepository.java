3c05ef05378160ab9ad8f40a4921841ec3e396ac
penzata86 <penzata86@gmail.com>
2022-06-27 19:37:12 +0300

    KB-11 - Get all manufacturers API

--- src/main/java/com/karucabg/demo/persistence/repository/ManufacturerRepository.java
+++ src/main/java/com/karucabg/demo/persistence/repository/ManufacturerRepository.java
@@ -4,7 +4,10 @@ import com.karucabg.demo.domain.model.Manufacturer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ManufacturerRepository extends CrudRepository<Manufacturer, Integer> {
 
+    List<Manufacturer> findAll();
 }
